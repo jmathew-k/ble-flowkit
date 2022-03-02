e8cc618db856c3791d19e7ecb79c502dde34aa8d
Jackson Keating <jackson.keating@gmail.com>
2022-03-02 00:04:36 -0600

    add descriptor/interpret to dnd.

--- src/DND-flow/Sidebar.tsx
+++ src/DND-flow/Sidebar.tsx
@@ -1,11 +1,15 @@
 import React, { DragEvent } from 'react'
 
-const onDragStart = (event: DragEvent, nodeType: string) => {
+const onDragStart = (event: DragEvent, nodeType: string, commandType: string) => {
 	event.dataTransfer.setData('application/reactflow', nodeType)
+	event.dataTransfer.setData('application/reactflowcommand', commandType)
 	event.dataTransfer.effectAllowed = 'move'
 }
 
 const Sidebar = () => {
+	var connectFunction = () => {
+		console.log("connect")
+	}
 	return (
 		<aside>
 			<div className='description'>
@@ -13,35 +17,35 @@ const Sidebar = () => {
 			</div>
 			<div
 				className='react-flow__node-input'
-				onDragStart={(event: DragEvent) => onDragStart(event, 'input')}
+				onDragStart={(event: DragEvent) => onDragStart(event, 'input', 'connect')}
 				draggable
 			>
 				Connect Bluetooth
 			</div>
 			<div
 				className='react-flow__node-default'
-				onDragStart={(event: DragEvent) => onDragStart(event, 'default')}
+				onDragStart={(event: DragEvent) => onDragStart(event, 'default', 'test1')}
 				draggable
 			>
 				BLE Test Sequence 1
 			</div>
 			<div
 				className='react-flow__node-default'
-				onDragStart={(event: DragEvent) => onDragStart(event, 'default')}
+				onDragStart={(event: DragEvent) => onDragStart(event, 'default', 'test2')}
 				draggable
 			>
 				BLE Test Sequence 2
 			</div>
 			<div
 				className='react-flow__node-default'
-				onDragStart={(event: DragEvent) => onDragStart(event, 'default')}
+				onDragStart={(event: DragEvent) => onDragStart(event, 'default', 'test3')}
 				draggable
 			>
 				BLE Test Sequence 3
 			</div>
 			<div
 				className='react-flow__node-output'
-				onDragStart={(event: DragEvent) => onDragStart(event, 'output')}
+				onDragStart={(event: DragEvent) => onDragStart(event, 'output', 'stop')}
 				draggable
 			>
 				Stop Transmission
--- src/DND-flow/dnd.tsx
+++ src/DND-flow/dnd.tsx
@@ -65,11 +65,40 @@ const DnDFlow = () => {
 		console.log("run")
 	}
 
+	var connectBluetooth = () => {
+		console.log("connect")
+	}
+	var test1Bluetooth = () => {
+		console.log("test1")
+	}
+	var test2Bluetooth = () => {
+		console.log("test2")
+	}
+	var test3Bluetooth = () => {
+		console.log("test3")
+	}
+	var stopBluetooth = () => {
+		console.log("stop")
+	}
+
+	var table:object = {
+		connect: {label: 'connect', runtime: connectBluetooth},
+		test1: {label: 'test1', runtime: test1Bluetooth},
+		test2: {label: 'test2', runtime: test2Bluetooth},
+		test3: {label: 'test3', runtime: test3Bluetooth},
+		stop: {label: 'stop', runtime: stopBluetooth},
+	}
+
+	var dataLookup = (type:string) => {
+		return table[type as keyof typeof table]
+	}
+
 	const onDrop = (event: DragEvent) => {
 		event.preventDefault()
 
 		if (reactFlowInstance) {
 			const type = event.dataTransfer.getData('application/reactflow')
+			const typeCommand = event.dataTransfer.getData('application/reactflowcommand')
 			const position = reactFlowInstance.project({
 				x: event.clientX,
 				y: event.clientY - 40,
@@ -78,7 +107,7 @@ const DnDFlow = () => {
 				id: getId(),
 				type,
 				position,
-				data: { label: `${type} node`, runtime: runFunction},
+				data: dataLookup(typeCommand),
 			}
 
 			setElements((es) => es.concat(newNode))
